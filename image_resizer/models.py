ad3627f41a4bfdf14d4ca14baf4d3179ed4451ac
Nevmetov Azat <nevmetov.azat@gmail.com>
2020-06-21 18:26:50 +0300

    Chamged id from AutoField to UUIDField

--- image_resizer/models.py
+++ image_resizer/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from image_resizer.types import StatusTypes
+import uuid
 
 # Create your models here.
 
 class ResizeTask(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     status = models.CharField(choices=StatusTypes.choices, max_length=15,
                               default=StatusTypes.NOT_STARTED,
                               verbose_name='Статус обработки задачи')
     target_width = models.PositiveSmallIntegerField(verbose_name='Ширина для ресайза')
     target_height = models.PositiveSmallIntegerField(verbose_name='Высота для ресайза')
-    img = models.FileField(verbose_name='Файл картинки')
+    img = models.ImageField(verbose_name='Файл картинки')
